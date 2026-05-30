5a81d07a46e8f689c85672fe3ed4c89caba87b4d
IamMachir <marveltechofficial@gmail.com>
2026-05-30 19:13:08 +0300

    Remove original (moved to Machir_Tadesse_UGE27638_14/)

--- exercises/part1-basics/SignedInt.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-    int8  public a          =  50;
-    int8  public b          = -30;
-    int16 public difference = a - b;
-}
