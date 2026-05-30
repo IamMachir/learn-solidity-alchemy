b90e8a7cc12e6ec1d1815f6615c5135f8882dd30
IamMachir <marveltechofficial@gmail.com>
2026-05-30 19:13:09 +0300

    Remove original (moved to Machir_Tadesse_UGE27638_14/)

--- exercises/part1-basics/Strings.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-    bytes32 public msg1 = "Hello World";
-    string  public msg2 = "This is a long string that needs more than 32 bytes of storage.";
-}
